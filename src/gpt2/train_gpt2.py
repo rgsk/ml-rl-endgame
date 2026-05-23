166e8a942e793fbbc293a379d9eaecf1f86032aa
rgsk <guptarahul70322@gmail.com>
2026-05-23 17:56:36 +0530

    autodetect device, and switch to a random model

--- src/gpt2/train_gpt2.py
+++ src/gpt2/train_gpt2.py
@@ -201,12 +201,21 @@ class GPT(nn.Module):
 
 
 # -----------------------------------------------------------------------------
+# attempt to autodetect the device
+device = "cpu"
+if torch.cuda.is_available():
+    device = "cuda"
+elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+    device = "mps"
+print(f"using device: {device}")
+
 num_return_sequences = 5
 max_length = 30
 
-model = GPT.from_pretrained("gpt2")
+# model = GPT.from_pretrained("gpt2")
+model = GPT(GPTConfig())
 model.eval()
-model.to("cuda")
+model.to(device)
 
 # prefix tokens
 
@@ -214,7 +223,7 @@ enc = tiktoken.get_encoding("gpt2")
 tokens = enc.encode("Hello, I'm a language model,")
 tokens = torch.tensor(tokens, dtype=torch.long)  # (8,)
 tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)  # (5, 8)
-x = tokens.to("cuda")
+x = tokens.to(device)
 
 # generate! right now x is (B, T) where B = 5, T = 8
 # set the seed to 42
@@ -258,7 +267,7 @@ def test_hf_model():
 
     model = GPT2LMHeadModel.from_pretrained("gpt2")
     model.eval()
-    model.to("cuda")
+    model.to(device)
 
     # prefix tokens
 
@@ -266,7 +275,7 @@ def test_hf_model():
     tokens = enc.encode("Hello, I'm a language model,")
     tokens = torch.tensor(tokens, dtype=torch.long)  # (8,)
     tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)  # (5, 8)
-    x = tokens.to("cuda")
+    x = tokens.to(device)
 
     # generate! right now x is (B, T) where B = 5, T = 8
     # set the seed to 42
@@ -297,6 +306,3 @@ def test_hf_model():
         tokens = x[i].tolist()
         decoded = enc.decode(tokens)
         print(">", decoded)
-
-
-test_hf_model()
