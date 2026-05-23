f5696e36ec510187f3c96611f195019a0b182adb
rgsk <guptarahul70322@gmail.com>
2026-05-24 01:53:15 +0530

    add learning rate scheduler

--- src/gpt2/train_gpt2.py
+++ src/gpt2/train_gpt2.py
@@ -1,3 +1,4 @@
+import math
 import time
 from dataclasses import dataclass
 from typing import cast
@@ -351,9 +352,31 @@ def train():
 
     train_loader = DataLoaderLite(B=2, T=1024)
 
-    optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+    optimizer = torch.optim.AdamW(
+        model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-8
+    )
 
-    for i in range(50):
+    max_lr = 6e-4
+    min_lr = max_lr * 0.1
+    warmup_steps = 10
+    max_steps = 50
+
+    def get_lr(it):
+        # 1) linear warmup for warmup_iters steps
+        if it < warmup_steps:
+            return max_lr * (it + 1) / warmup_steps
+        # 2) if it > lr_decay_iters, return min learning rate
+        if it > max_steps:
+            return min_lr
+        # 3) in between, use cosine decay down to min learning rate
+        decay_ratio = (it - warmup_steps) / (max_steps - warmup_steps)
+        assert 0 <= decay_ratio <= 1
+        coeff = 0.5 * (
+            1.0 + math.cos(math.pi * decay_ratio)
+        )  # coeff starts at 1 and goes to 0
+        return min_lr + coeff * (max_lr - min_lr)
+
+    for step in range(max_steps):
         if device == "cuda":
             torch.cuda.synchronize()
         t0 = time.time()
@@ -365,6 +388,11 @@ def train():
 
         assert loss is not None
         loss.backward()
+        norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+        # determine and set the learning rate for this iteration
+        lr = get_lr(step)
+        for param_group in optimizer.param_groups:
+            param_group["lr"] = lr
         optimizer.step()
         if device == "cuda":
             torch.cuda.synchronize()
@@ -372,7 +400,7 @@ def train():
         dt = (t1 - t0) * 1000
         tokens_per_sec = (train_loader.B * train_loader.T) / (t1 - t0)
         print(
-            f"step {i}, loss: {loss.item()}, dt: {dt:.2f}ms, tok/sec: {tokens_per_sec:.2f}"
+            f"step {step:4d} | loss: {loss.item():.6f} | lr {lr:.4e} | norm: {norm:.4f} | dt: {dt * 1000:.2f}ms | tok/sec: {tokens_per_sec:.2f}"
         )
 
 
